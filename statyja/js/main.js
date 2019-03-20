261c10e05e42136680fc9832d077c45209e5075e
RomaJulik <48775886+RomaJulik@users.noreply.github.com>
2019-03-21 00:56:22 +0100

    создал страничку статуи
    закал папки и файлы

---
+++ statyja/js/main.js
@@ -0,0 +1,83 @@
+$(function(){
+    $(".glitch-boy").mgGlitch({
+        destroy: false,
+        glitch: true,
+        scale: true,
+        blend: true,
+        blendModeType: 'hue',
+        glitch1TimeMin: 200,
+        glitch1TimeMax: 400,
+        glitch2TimeMin: 10,
+        glitch2TimeMax: 100,                
+    });
+});
+
+$(function(){
+    $(".glitch-girl").mgGlitch({
+        destroy: false,
+        glitch: true,
+        scale: true,
+        blend: true,
+        blendModeType: 'hue',
+        glitch1TimeMin: 200,
+        glitch1TimeMax: 400,
+        glitch2TimeMin: 10,
+        glitch2TimeMax: 100,                
+    });
+});
+
+
+
+$(document).ready(function() {
+    
+    $('#neonthis').novacancy({
+    
+    'reblinkProbability' : 0.6,
+    'blinkMin' : 0.1,
+    'blinkMax' : 0.4,
+    'loopMin' : 0.4,
+    'loopMax' : 1,
+    'color' : 'pink',
+    'glow' : ['0 0 80px pink', '0 0 30px pink', '0 0 6px blue'],
+    'blink' : 0,
+    'autoOn' : true
+    });
+        
+        
+    });
+
+$(document).ready(function() {
+    
+    $('#neon').novacancy({
+    
+    'reblinkProbability' : 0.6,
+    'blinkMin' : 0.1,
+    'blinkMax' : 0.4,
+    'loopMin' : 0.4,
+    'loopMax' : 1,
+    'color' : 'pink',
+    'glow' : ['0 0 80px pink', '0 0 30px pink', '0 0 6px blue'],
+    'blink' : 0,
+    'autoOn' : true
+    });
+        
+        
+    });
+
+$(document).ready(function() {
+    
+    $('#neonspan').novacancy({
+    
+    'reblinkProbability' : 0.6,
+    'blinkMin' : 0.1,
+    'blinkMax' : 0.4,
+    'loopMin' : 0.4,
+    'loopMax' : 1,
+    'color' : 'pink',
+    'glow' : ['0 0 80px pink', '0 0 30px pink', '0 0 6px blue'],
+    'blink' : 0,
+    'autoOn' : true
+    });
+        
+        
+    });
